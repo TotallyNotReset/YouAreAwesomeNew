3b3c868151e25e0752267a817d22f57dca283b87
Nicolo <nicoloissuchagoofball@gmail.com>
2025-02-10 11:28:45 -0800

    Initial Commit

---
+++ YouAreAwesomeNew/YouAreAwesomeNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeNewApp.swift
+//  YouAreAwesomeNew
+//
+//  Created by Student1 on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
